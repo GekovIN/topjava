fec95b50bd2ad172557496c24829dcadbfc4a6f0
Gekov Iliya <gekovin@gmail.com>
2018-10-16 00:39:58 +0300

    HW2.5 done - MealService Spring integration

--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.mock.InMemoryMealRepositoryImpl;
@@ -10,11 +12,12 @@ import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class MealServiceImpl implements MealService {
-
 
     private MealRepository repository;
 
+    @Autowired
     public MealServiceImpl(MealRepository repository) {
         this.repository = repository;
     }
