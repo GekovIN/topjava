488b3e2f9d0dbaaaebf9ee32ea73bddafec56fa9
Gekov Iliya <gekovin@gmail.com>
2018-11-05 14:28:50 +0300

    HW5 Optional .6 done - SpringMain and MealServlet fix using profiles

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -17,18 +17,24 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/inmemory.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
-            System.out.println();
-
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealTo> filteredMealsWithExcess =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExcess.forEach(System.out::println);
+        try (ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext()) {
+            appCtx.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.JDBC);
+            appCtx.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+            appCtx.refresh();
+            System.out.println("Bean definition names: ");
+            for (String bean : appCtx.getBeanDefinitionNames()) {
+                System.out.println(bean);
+            }
+//            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+//            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+//            System.out.println();
+//
+//            MealRestController mealController = appCtx.getBean(MealRestController.class);
+//            List<MealTo> filteredMealsWithExcess =
+//                    mealController.getBetween(
+//                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+//                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+//            filteredMealsWithExcess.forEach(System.out::println);
         }
     }
 }
